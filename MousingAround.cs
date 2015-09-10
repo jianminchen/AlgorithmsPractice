14fe99378a15ac56c98106784567c351d95d151c
jianminchen <chenjulia1@users.noreply.github.com>
2015-09-10 16:43:05 -0700

    change the layout of comment for easy to read

--- MousingAround.cs
+++ MousingAround.cs
@@ -49,22 +49,46 @@ using System.Threading.Tasks;
 /*
  * 
  * comment I like most from the blog:
- * Fortunately, that's not the way it works here either. Finding a correct solution to the problem is not the end goal. In an interview I assume you can do that. What is more important to me as an interviewer is what I learn about you as you attempt to solve it. Like, do you communicate well. Do you test your assumptions? Do you have any assumptions? Do you believe in your solution? Can you apply what you've just learned to a follow up question/scenario? Do you identify the full problem before plunging into the deep end? Are you able to back out of a dead end? Do you recognize when you are at a dead end?
+ * Fortunately, that's not the way it works here either. Finding a correct solution to the problem is not 
+ * the end goal. In an interview I assume you can do that. What is more important to me as an interviewer 
+ * is what I learn about you as you attempt to solve it. Like, do you communicate well. Do you test your assumptions? 
+ * Do you have any assumptions? Do you believe in your solution? Can you apply what you've just learned to 
+ * a follow up question/scenario? Do you identify the full problem before plunging into the deep end? Are you 
+ * able to back out of a dead end? Do you recognize when you are at a dead end?
    
  * Analysis from the blog:
  * 
  * http://blogs.msdn.com/b/mattwar/archive/2005/02/11/371274.aspx
  * 
- * lets discuss the problem again so we can understand the solutions in context.  A robot mouse is dropped into a maze where somewhere there is placed a piece of cheese.  You must write an algorithm to control the mouse and successfully find the cheese.  Unfortunately, the mouse is blind so the only feedback you have is the paltry information coming back from the mouse 'interface'.  The maze is a regular grid of cells, where each cells may have zero or more walls that impede movement, except for the outer edge that is completely sealed.   At the start you don't know where the walls are, you don't know where the cheese is and you definitely don't know where the mouse is.  All you do know that the maze is limited to some particular size.
+ * lets discuss the problem again so we can understand the solutions in context.  A robot mouse is dropped 
+ * into a maze where somewhere there is placed a piece of cheese.  You must write an algorithm to control 
+ * the mouse and successfully find the cheese.  Unfortunately, the mouse is blind so the only feedback you 
+ * have is the paltry information coming back from the mouse 'interface'.  The maze is a regular grid of cells, 
+ * where each cells may have zero or more walls that impede movement, except for the outer edge that is completely 
+ * sealed.   
+ * 
+ * At the start you don't know where the walls are, you don't know where the cheese is and you definitely don't 
+ * know where the mouse is.  All you do know that the maze is limited to some particular size.
 
-What this should tell you is that you need to exhaustively and deterministically search the maze, stopping only when you find the cheese.  What it doesn't tell you is the pitfalls you may encounter when doing so, things like cycles, paths that lead back onto themselves, and open expanses that have no walls around them at all.  A successful solution will avoid these areas of trouble.  A good solution will do it in a nominal number of moves.
+   What this should tell you is that you need to exhaustively and deterministically search the maze, stopping only 
+ * when you find the cheese.  What it doesn't tell you is the pitfalls you may encounter when doing so, things like 
+ * cycles, paths that lead back onto themselves, and open expanses that have no walls around them at all.  A successful 
+ * solution will avoid these areas of trouble.  A good solution will do it in a nominal number of moves.
 
-    You must avoid walking randomly.  There is no way of knowing whether the series of suggested directions will actually cause you to traverse the entire maze.   You may just bob back and forth forever.
-    You must avoid walking in circles.  You need to employ some mechanism to remember where you have been.  Since the mouse cannot tell you where it is or where it has been, and offers no model of the world other than success or failure to move, you must invent your own model and a means to keep track. 
-    You must avoid getting stuck at a dead end.  Going back the way you came is a good idea, but make sure you have a way to keep going, that path up to the end might have been narrow and long.
-    At the edges of your explored space will be all the places you have not yet been.  In order to exhaustively search the maze you need a way to get back there and try them.
+   You must avoid walking randomly.  There is no way of knowing whether the series of suggested directions will actually 
+ * cause you to traverse the entire maze.   You may just bob back and forth forever.
+ * 
+   You must avoid walking in circles.  You need to employ some mechanism to remember where you have been.  Since the mouse 
+ * cannot tell you where it is or where it has been, and offers no model of the world other than success or failure to move, 
+ * you must invent your own model and a means to keep track. 
+   
+ * You must avoid getting stuck at a dead end.  Going back the way you came is a good idea, but make sure you have a way to 
+ * keep going, that path up to the end might have been narrow and long.
+ * 
+   At the edges of your explored space will be all the places you have not yet been.  In order to exhaustively search the 
+ * maze you need a way to get back there and try them.
 
-So successful solutions employs cycle detection and backtracking.  
+   So successful solutions employs cycle detection and backtracking.  
  */
 namespace MousingAround
 {
@@ -96,7 +120,8 @@ namespace MousingAround
          * http://blogs.msdn.com/b/mattwar/archive/2005/02/11/371274.aspx
          * 
          * julia's comment: 
-         * 
+         *  1. try to identify the cycle detection / back tracking code
+         *  2. Try to understand code. 
          */
         public static bool FindCheese(IMouse mouse, int rows, int cols) {
 
@@ -124,7 +149,6 @@ namespace MousingAround
                     int nr = (r + dr[d] + rows) % rows;
 
                     int nc = (c + dc[d] + cols) % cols;
-
                    
 
                     // only try to move to cells we have not already visited
@@ -136,21 +160,16 @@ namespace MousingAround
                         c = nc; 
 
                         // remember how to get back
-
                         grid[r, c].prev = (byte)((d + 2) % 4);
-
                     }
 
                 }
-
                 else {
 
                     // backtrack
-
                     d = grid[r, c].prev;
 
                     if (d == 4)
-
                         return false;
 
                     mouse.Move((Direction)d);
@@ -158,9 +177,7 @@ namespace MousingAround
                     r = (r + dr[d] + rows) % rows;
 
                     c = (c + dc[d] + cols) % cols;
-
                 }
-
             } 
 
             return true;
@@ -169,7 +186,6 @@ namespace MousingAround
         static int[] dr = new int[] { -1, 0, 1, 0 };
 
         static int[] dc = new int[] { 0, 1, 0, -1 };
-
     
     }
 }
